b73eef6dcb01ee07285a7b651232a699cbefaa19
Laszlo Starost <laszlo.starost@barmer.de>
2025-11-19 00:23:08 +0100

    refactor: Remove duplicated className in Button.tsx

--- src/components/intern/Button.tsx
+++ src/components/intern/Button.tsx
@@ -22,7 +22,7 @@ export const Button = ({
   ...props
 }: ButtonProps) => {
   const className = primary
-    ? `inline-flex justify-center rounded-full text-${size} font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 bg-gray-950 text-white hover:bg-gray-800 focus-visible:outline-gray-950 dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-white dark:hover:bg-gray-200 dark:focus-visible:outline-white dark:focus-visible:outline-white px-4 py-2`
+    ? `inline-flex justify-center rounded-full text-${size} font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 bg-gray-950 text-white hover:bg-gray-800 focus-visible:outline-gray-950 dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-white dark:hover:bg-gray-200 dark:focus-visible:outline-white px-4 py-2`
     : `inline-flex justify-center rounded-full text-${size} font-semibold ring-1 text-gray-950 ring-gray-950/10 hover:ring-gray-950/20 dark:text-white dark:ring-white/10 dark:hover:ring-white/20 px-4 py-2`;
 
   return (
